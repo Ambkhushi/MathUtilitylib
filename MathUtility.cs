55c32d7fb1909311b896714e6762eeea52309709
gsmca128@sgsitsindore.in <gsmca128@sgsitsindore.in>
2023-07-21 17:13:07 +0530

    Add project files.

---
+++ MathUtility.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace MathUtilitylibr
+{
+    public class MathUtility
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+        public int Multi(int a, int b)
+        {
+            return a * b;
+        }
+        public int Divide(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Cannot divide by zero.");
+            }
+            return a / b;
+        }
+        public bool IsPrime(int number)
+        {
+            if (number <= 1)
+            {
+                return false;
+            }
+            for(int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
